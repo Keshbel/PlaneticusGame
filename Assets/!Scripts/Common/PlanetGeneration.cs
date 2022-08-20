3dfd137be83438196db042fad2f490f7ee9be3e8
Keshbel <atari111777@gmail.com>
2022-08-20 20:53:21 +0700

    First Commit

---
+++ Assets/!Scripts/Common/PlanetGeneration.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGeneration : MonoBehaviour
+{
+    public GameObject planetPrefab;
+    
+    public void Generation()
+    {
+        var countPlanet = Random.Range(20, 25);
+        
+        
+    }
+}
